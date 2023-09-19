1f54cb6f36768ac19fde8ab86cf7fb037c0d8ed3
Haroonawan1 <haroona13@nycstudents.net>
2023-09-19 13:05:27 -0400

    Added a print statement about my favorite food.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Haroon Awan");
-        System.out.println("Morbuis");
+        System.out.println("Morbius");
+        System.out.println("ice cream");
         }
     }
